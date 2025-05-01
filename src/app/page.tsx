13a7419be00d9d55e1a392d28348eefe6aa3ed64
sammythedeveloper <justsammy484@gmail.com>
2025-04-30 22:25:48 -0400

    service page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Landing } from "@/sections/Landing";
 import { Collection } from "@/sections/Collection";
 import { Header } from "@/sections/Header";
+import { Service } from "@/sections/Service";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Landing />
       <Collection />
+      <Service/>
     </div>
   );
 }
---
+++ src/sections/Service.tsx
@@ -0,0 +1,16 @@
+export const Service = () => {
+  return (
+    <div className="py-16 lg:py-24">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center py-4">
+          <div>HAIRCUT</div>
+          <div>SHAVE</div>
+          <div>SILK PRESS</div>
+          <div>COSMETICS</div>
+          <div>BRAIDS</div>
+          <div>VIP GROOMING</div>
+        </div>
+      </div>
+    </div>
+  );
+};
