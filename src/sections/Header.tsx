1bd889acbe169aa2ec236d85d65f158e0626f65e
sammythedeveloper <justsammy484@gmail.com>
2025-05-03 21:04:10 -0400

    css arangement

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,20 +2,19 @@ import Scissors from "@/assets/images/scissors.svg";
 export const Header = () => {
   return (
     <div className="sticky top-0 z-40 backdrop-blur-lg">
-      <div className="container">
+      <div className="container mb-7  ">
         <div className="flex justify-between items-center h-24 md:h-28">
-          <div className=" flex  font-heading ml-12 text-4xl font-bold">
+          <div className=" flex  font-heading  text-xl font-bold">
             <Scissors className=" w-10 h-10 text-blue-500 " />
-            <h1 className=" ml-6 ">Four Season </h1>
+            <h2 className=" ml-6 ">Four Season </h2>
           </div>
-          <div className="flex items-center gap-4">
-            {/* Links visible on large screens */}
-            <div className="flex gap-4 items-center">
-              <div className="hidden md:inline-flex">Service & Pricing </div>
+          <div className="flex items-center gap-2 text-sm whitespace-nowrap">
+            <div className="flex gap-2 items-center">
+              <div className="hidden md:inline-flex">Service & Pricing</div>
               <div className="hidden md:inline-flex">Location</div>
               <div className="hidden md:inline-flex">Studio Membership</div>
               <div className="hidden md:inline-flex">Contact Us</div>
-              <div className="hidden md:inline-flex bg-white text-black  rounded-full py-2 px-6 hover:bg-black hover:text-white ">
+              <div className="hidden md:inline-flex bg-white text-black rounded-full py-1.5 px-4 hover:bg-black hover:text-white">
                 Book Now
               </div>
             </div>
--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -1,7 +1,7 @@
 import AboutIcon from "@/assets/images/download.svg";
 export const Landing = () => {
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center bg-white relative overflow-hidden">
+    <section className="min-h-screen flex flex-col items-center justify-center bg-white relative overflow-hidden ">
       {/* Grid Background */}
       <div
         className="absolute inset-0 rotate-[75deg] bg-white 
@@ -15,13 +15,18 @@ export const Landing = () => {
       ></div>
       <div
         className="absolute inset-0 before:absolute before:inset-0 before:animate-[pulsegrid_6s_ease-in-out_infinite] 
-          before:bg-[radial-gradient(circle,#e5e7eb33_1px,transparent_1px)] before:bg-[size:40px_40px] before:opacity-10"
+          before:bg-[radial-gradient(circle,#e5e7eb33_1px,transparent_1px)] before:bg-[size:40px_40px] before:opacity-10 "
       ></div>
       {/* Main Content */}
-      <div className="z-10 text-center px-4">
-        <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-black">
-          Four Season Barbershop
-        </h1>
+      <div className="z-10 max-w-2xl w-full px-6 text-center flex flex-col items-center justify-center space-y-12">
+        <p className="text-4xl md:text-6xl font-extrabold text-black">
+          WE ARE FOUR SEASON STUDIOS
+        </p>
+        <p className=" text-center justify-center ">
+          With a diverse team of skilled barbers, we offer a level of expertise
+          and individuality that sets us apart. At Four Season Studios, it’s
+          about more than just a haircut – it’s about setting a new standard.
+        </p>
         <AboutIcon className=" text-blue-500" />
         <p className="mt-4 text-xl sm:text-2xl text-gray-600">Men care Salon</p>
       </div>
