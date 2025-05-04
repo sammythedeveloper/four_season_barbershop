dba0bf80847064b1072f821eda5c2bc15c86c59d
sammythedeveloper <justsammy484@gmail.com>
2025-05-03 21:31:19 -0400

    style

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,23 +1,27 @@
 import Scissors from "@/assets/images/scissors.svg";
+import MenuIcon from "@mui/icons-material/Menu";
 export const Header = () => {
   return (
     <div className="sticky top-0 z-40 backdrop-blur-lg">
       <div className="container mb-7  ">
         <div className="flex justify-between items-center h-24 md:h-28">
-          <div className=" flex  font-heading  text-xl font-bold">
-            <Scissors className=" w-10 h-10 text-blue-500 " />
-            <h2 className=" ml-6 ">Four Season </h2>
+          <div className=" flex items-center text-base font-bold  ">
+            <p><Scissors className=" w-10 h-10  " /></p>
+            <p className=" ml-4 ">Four Season </p>
           </div>
-          <div className="flex items-center gap-2 text-sm whitespace-nowrap">
-            <div className="flex gap-2 items-center">
-              <div className="hidden md:inline-flex">Service & Pricing</div>
-              <div className="hidden md:inline-flex">Location</div>
-              <div className="hidden md:inline-flex">Studio Membership</div>
-              <div className="hidden md:inline-flex">Contact Us</div>
-              <div className="hidden md:inline-flex bg-white text-black rounded-full py-1.5 px-4 hover:bg-black hover:text-white">
+          <div className="hidden md:flex items-center gap-4 text-sm whitespace-nowrap">
+            <div className="flex gap-4 items-center">
+              <div>Service & Pricing</div>
+              <div>Location</div>
+              <div>Studio Membership</div>
+              <div>Contact Us</div>
+              <div className="bg-white text-black rounded-full py-1.5 px-4 hover:bg-black hover:text-white">
                 Book Now
               </div>
             </div>
+          </div>
+          <div className="md:hidden">
+            <MenuIcon className="w-8 h-8 text-black cursor-pointer" />
           </div>
         </div>
       </div>
