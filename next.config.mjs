c0a67e6bc32e70d0880df61e25b389f919c4be8e
sammythedeveloper <justsammy484@gmail.com>
2025-05-31 01:35:53 -0400

    hhq

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,20 @@
 const isProd = process.env.NODE_ENV === "production";
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Required for static export
+  output: "export",
   basePath: isProd ? "/four_season_barbershop" : "",
   assetPrefix: isProd ? "/four_season_barbershop/" : "",
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true,
   },
-  // Support importing .svg files as React components
-  webpack(config, { isServer }) {
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
     });
-
     return config;
   },
 };
 
 export default nextConfig;
-
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/**/*.html",         // For any static HTML in public
-    "./out/**/*.html",            // ✅ Needed for Netlify static build output
-    "./out/**/*.js",              // ✅ JS output from next export (tailwind might purge otherwise)
+    "./public/**/*.html",      
   ],
   theme: {
     screens: {
